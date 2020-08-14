39428e9563a1c5bd57c01c54f3fb673e7d2761c8
Michael Odusanya <modusanya12@gmail.com>
2020-08-14 17:56:11 -0400

    initial bot commands

---
+++ index.js
@@ -0,0 +1,86 @@
+const Discord = require('discord.js');
+const {Client, MessageAttachment} = require('discord.js');
+const bot = new Discord.Client();
+const cheerio = require('cheerio');
+const request = require('request');
+
+const token = 'NzQzNTU0NzM1ODE0MDgyNzAx.XzWXTw.Y_MiRTa7mYrFCDrjo_05wiNkfKE';
+const PREFIX = '$';
+
+bot.on('ready', () => {
+    console.log("The bot is indeed in this bitch!");
+})
+
+bot.on('message', msg=>{
+    if(!msg.content.startsWith(PREFIX)) return;
+    let args = msg.content.substring(PREFIX.length).split(" ");
+
+    switch(args[0]){
+        case 'ping':
+            msg.channel.send('ping me again nigga :gun:');
+            break;
+        case 'plug':
+            if(args[1]==='twitter'){
+                msg.channel.send('https://twitter.com/MichaelOdusanya');
+            }else if(args[1]==='github'){
+                msg.channel.send('https://github.com/dengaku12');
+            }else if(args[1]==='linkedin'){
+                msg.channel.send('https://www.linkedin.com/in/michael-odusanya/');
+            }else{
+                msg.channel.send('wtf am i pluggin bro? I dont recognize this shit :( ')
+            }
+            break;
+        case 'userInfo':
+            const embed = new Discord.MessageEmbed()
+            .setTitle("User Info")
+            .addField('Player Name', msg.author.username)
+            .addField('Server Name', msg.guild.name)
+            .setColor(0xff33f6)
+            .setThumbnail(msg.author.displayAvatarURL())
+            msg.channel.send(embed);
+            break;
+        case 'bless':
+            image(msg, args[1]);
+            break;
+        case 'sosa':
+            msg.channel.send("Fuckers in school telling me, always in the barber shop Chief Keef ain’t bout this, Chief Keef ain’t bout that My boy a BD on fucking Lamron and them He, he they say that nigga don’t be putting in no work SHUT THE FUCK UP! Y'all niggas ain’t know shit All ya motherfuckers talk about Chief Keef ain’t no hitta Chief Keef ain’t this Chief Keef a fake SHUT THE FUCK UP Y'all don’t live with that nigga Y'all know that nigga got caught with a ratchet Shootin' at the police and shit Nigga been on probation since fuckin, I don’t know when! Motherfuckers stop fuckin' playin' him like that Them niggas savages out there If I catch another motherfucker talking sweet about Chief Keef I’m fucking beating they ass! I’m not fucking playing no more You know those niggas role with Lil' Reese and them");
+            break;
+        case 'gintoki':
+            const attach = new MessageAttachment("https://media.giphy.com/media/kdM3zfq85XSb6/giphy.gif");
+            msg.channel.send(attach);
+            break;
+    }
+});
+
+function image(msg, input){
+    var options = {
+        url: "http://results.dogpile.com/serp?qc=images&q=" + input,
+        method: "GET",
+        headers: {
+            "Accept": "text/html",
+            "User-Agent": "Chrome"
+        }
+    };
+
+    request(options, function(error, response, responseBody){
+        if(error){
+            return;
+        }
+
+        $ = cheerio.load(responseBody);
+
+        var links = $(".image a.link");
+
+        var urls = new Array(links.length).fill(0).map((v,i) => links.eq(i).attr("href"));
+        console.log(urls);
+
+        if(!urls.length){
+            return;
+        }
+
+        msg.channel.send(urls[Math.floor(Math.random()*urls.length)]);
+
+    });
+}
+
+bot.login(token);
